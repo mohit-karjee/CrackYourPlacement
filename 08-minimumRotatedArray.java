b555d8f3e82e2e4fdb27bc3b7dd5548459ccf4ce
mohit-karjee <mohit.karjee@gmail.com>
2023-07-04 02:10:22 +0530

    minimum search in rotated array

---
+++ 08-minimumRotatedArray.java
@@ -0,0 +1,19 @@
+package CrackYourPlacement;
+
+class Solution {
+    public int findMin(int[] nums) {
+        return findMinRecursive(nums,0,nums.length-1);
+    }
+    private int findMinRecursive(int[] nums,int left,int right){
+        
+        if(nums[left]==nums[right])
+        return nums[left];
+        int mid=left+(right-left)/2;
+        if(nums[mid]>nums[right]){
+            return findMinRecursive(nums,mid+1,right);
+        }else{
+            return findMinRecursive(nums,left,mid);
+        }
+
+    }
+}
