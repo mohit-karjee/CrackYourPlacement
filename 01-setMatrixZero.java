bc76eef22f11f1c2af44fa377e8a380a463d0e1b
mohit-karjee <mohit.karjee@gmail.com>
2023-07-03 19:14:20 +0530

    setmatrix

---
+++ 01-setMatrixZero.java
@@ -0,0 +1,57 @@
+package CrackYourPlacement;
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+          boolean fr = false,fc = false;
+    for(int i = 0; i < matrix.length; i++) {
+        for(int j = 0; j < matrix[0].length; j++) {
+            if(matrix[i][j] == 0) {
+                if(i == 0) fr = true;
+                if(j == 0) fc = true;
+                matrix[0][j] = 0;
+                matrix[i][0] = 0;
+            }
+        }
+    }
+    for(int i = 1; i < matrix.length; i++) {
+        for(int j = 1; j < matrix[0].length; j++) {
+            if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+                matrix[i][j] = 0;
+        }}
+    }
+    if(fr) {
+        for(int j = 0; j < matrix[0].length; j++) {
+            matrix[0][j] = 0;
+        }
+    }
+    if(fc) {
+        for(int i = 0; i < matrix.length; i++) {
+            matrix[i][0] = 0;
+        }
+    }
+    }
+   
+    public static void main(String[] args) {
+        // Example input matrix
+        int[][] matrix = {
+            {1, 1, 1},
+            {1, 0, 1},
+            {1, 1, 1}
+        };
+
+        // Create an instance of the Solution class
+        Solution solution = new Solution();
+
+        // Call the setZeroes method to modify the matrix
+        solution.setZeroes(matrix);
+
+        // Print the modified matrix
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
