fbfd64c232b08f090c29129ea8ab8e6f6e7c41c1
mohit-karjee <mohit.karjee@gmail.com>
2023-07-04 09:45:25 +0530

    move zeroes

---
+++ 11-moveZeros.java
@@ -0,0 +1,20 @@
+package CrackYourPlacement;
+
+class Solution{
+    public void moveZeroes(int[] nums) {
+        int idx=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0 ){
+                int temp=nums[i];
+                nums[i]=0;
+                nums[idx++]=temp;
+            }   
+        }
+    }
+        
+ }
+
+
+
+
+
